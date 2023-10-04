c520e0193476b6229bd94340b12ae171ba7e6be3
andreaz98 <azanni518@gmail.com>
2023-10-04 12:11:19 +0200

    _dropper, solves assert failed: block_trim_free tlsf.c

--- src/utils/deep-sleeping.c
+++ src/utils/deep-sleeping.c
@@ -25,6 +25,7 @@ int zp_prepare_to_sleep(z_owned_session_t zs){
     _serialize_z_resource_list_t(zs._value->_local_resources, local_resources);
     _serialize_z_resource_list_t(zs._value->_remote_resources, remote_resources);
 
+    //TODO check that serialize/deserialize for arg are not null if arg is not null
     _serialize_z_subscription_sptr_list_t(zs._value->_local_subscriptions, _write_subscription_local, local_subscriptions);
     _serialize_z_subscription_sptr_list_t(zs._value->_remote_subscriptions, _write_subscription_remote, remote_subscriptions);
 
@@ -39,6 +40,8 @@ z_owned_session_t zp_wake_up(){
         zs._value->_local_resources = _deserialize_z_resource_list_t(local_resources);
         zs._value->_remote_resources = _deserialize_z_resource_list_t(remote_resources);
 
+        memset(local_subscriptions, 0, DIM_LOCAL_SUBSCRIPTIONS);
+        memset(remote_subscriptions, 0, DIM_REMOTE_SUBSCRIPTIONS);
         zs._value->_local_subscriptions = _deserialize_z_subscription_sptr_list_t(local_subscriptions);
         zs._value->_remote_subscriptions = _deserialize_z_subscription_sptr_list_t(remote_subscriptions);
     }
@@ -49,7 +52,6 @@ z_owned_session_t zp_wake_up(){
 int _serialize_z_resource_list_t(_z_resource_list_t *list, uint8_t *resources){  
     int ret = _Z_RES_OK;  
     _z_resource_t *element;
-    
     size_t no_of_elements = _z_resource_list_len(list);
 
     uint8_t *_buffer = resources;
@@ -186,37 +188,17 @@ int _serialize_z_subscription_sptr_list_t(_z_subscription_sptr_list_t * list, in
         memcpy(_buffer, &element->ptr->_callback, 4); //cannot be NULL
         _buffer += 4;
 
-        size_t zero = 0;
-        if(element->ptr->_dropper != NULL){
-            memcpy(_buffer, &element->ptr->_dropper, 4);
-            _buffer += 4;
-        } else {
-            memcpy(_buffer, &zero, 4);
-            _buffer += 4;
-        }
+        if(element->ptr->_dropper != NULL) memcpy(_buffer, &element->ptr->_dropper, 4);
+        _buffer += 4;
 
-        if(element->ptr->serde_functions.serialize != NULL){
-            memcpy(_buffer, &element->ptr->serde_functions.serialize, 4);
-            _buffer += 4;
-        } else {
-            memcpy(_buffer, &zero, 4);
-            _buffer += 4;
-        }
+        if(element->ptr->serde_functions.serialize != NULL) memcpy(_buffer, &element->ptr->serde_functions.serialize, 4);
+        _buffer += 4;
 
-        if(element->ptr->serde_functions.deserialize != NULL){
-            memcpy(_buffer, &element->ptr->serde_functions.deserialize, 4);
-            _buffer += 4;
-        } else {
-            memcpy(_buffer, &zero, 4);
-            _buffer += 4;
-        }
+        if(element->ptr->serde_functions.deserialize != NULL) memcpy(_buffer, &element->ptr->serde_functions.deserialize, 4);
+        _buffer += 4;
 
-        if(element->ptr->_arg != NULL){
-            element->ptr->serde_functions.serialize(write, NULL, element->ptr->_arg);
-        } else {
-            memcpy(_buffer, &zero, 4);
-            _buffer += 4;
-        }
+        if(element->ptr->_arg != NULL) element->ptr->serde_functions.serialize(write, NULL, element->ptr->_arg);
+        _buffer += 4;
 
         iterate_list = _z_subscription_sptr_list_tail(iterate_list);
     }
@@ -227,10 +209,8 @@ int _serialize_z_subscription_sptr_list_t(_z_subscription_sptr_list_t * list, in
 _z_subscription_sptr_list_t * _deserialize_z_subscription_sptr_list_t(uint8_t * buffer){
     _z_subscription_sptr_list_t * list = _z_subscription_sptr_list_new();
     _z_subscription_sptr_t *element;
-
     size_t no_of_elements;
     uint8_t * _buffer = buffer;
-
 
     memcpy(&no_of_elements, _buffer, sizeof(size_t));
     _buffer += sizeof(size_t);
@@ -238,13 +218,12 @@ _z_subscription_sptr_list_t * _deserialize_z_subscription_sptr_list_t(uint8_t * 
     for (size_t i = 0; i < no_of_elements; i++)
     {
         element = (_z_subscription_sptr_t *) malloc(sizeof(_z_subscription_sptr_t));
-        //memset(element->ptr, 0, sizeof(element->ptr.));
+        memset(element->ptr, 0, sizeof(_z_subscription_t));
 
         //_key._id _key._mapping._val _key._suffix _id _info.period.origin _info.period.period _info.period.duration reliability mode _callback _dropper serialize deserialize _arg_len _arg
         element->ptr->_key = *((_z_keyexpr_t *)malloc(sizeof(_z_keyexpr_t)));
         memcpy(&element->ptr->_key._id, _buffer, sizeof(uint16_t));
         _buffer += sizeof(uint16_t);
-        printf("DEBUG element->ptr->_key._id: %d\n", element->ptr->_key._id);
 
         memcpy(&element->ptr->_key._mapping._val, _buffer, sizeof(uint16_t));
         _buffer += sizeof(uint16_t);
@@ -252,7 +231,6 @@ _z_subscription_sptr_list_t * _deserialize_z_subscription_sptr_list_t(uint8_t * 
         element->ptr->_key._suffix = malloc(strlen((char *)_buffer) + 1);
         memcpy(element->ptr->_key._suffix, _buffer, strlen((char *)_buffer) + 1);
         _buffer += strlen(element->ptr->_key._suffix) + 1;
-        printf("DEBUG element->ptr->_key._suffix: %s\n", element->ptr->_key._suffix);
 
         memcpy(&element->ptr->_id, _buffer, sizeof(uint32_t));
         _buffer += sizeof(uint32_t);
@@ -265,7 +243,6 @@ _z_subscription_sptr_list_t * _deserialize_z_subscription_sptr_list_t(uint8_t * 
 
         memcpy(&element->ptr->_info.period.duration, _buffer, sizeof(unsigned int));
         _buffer += sizeof(unsigned int);
-        printf("DEBUG element->ptr->_info.period.duration: %d\n", element->ptr->_info.period.duration);
 
         memcpy(&element->ptr->_info.reliability, _buffer, sizeof(z_reliability_t));
         _buffer += sizeof(z_reliability_t);
@@ -275,36 +252,20 @@ _z_subscription_sptr_list_t * _deserialize_z_subscription_sptr_list_t(uint8_t * 
 
         memcpy(&element->ptr->_callback, _buffer, 4); //cannot be NULL
         _buffer += 4;
-        printf("DEBUG element->ptr->_callback: %p\n", element->ptr->_callback);
 
         //_dropper serialize deserialize _arg_len _arg
         size_t tmp_maybe_address;
         memcpy(&tmp_maybe_address, _buffer, 4);
-        if(tmp_maybe_address == 0){
-            element->ptr->_dropper = NULL;
-            _buffer += 4;
-        } else {
-            memcpy(&element->ptr->_dropper, _buffer, 4);
-            _buffer += 4;
-        }
+        if(tmp_maybe_address != 0) memcpy(&element->ptr->_dropper, _buffer, 4);
+        _buffer += 4;
 
         memcpy(&tmp_maybe_address, _buffer, 4);
-        if(tmp_maybe_address == 0){
-            element->ptr->serde_functions.serialize = NULL;
-            _buffer += 4;
-        } else {
-            memcpy(&element->ptr->serde_functions.serialize, _buffer, 4);
-            _buffer += 4;
-        }
+        if(tmp_maybe_address != 0) memcpy(&element->ptr->serde_functions.serialize, _buffer, 4);
+        _buffer += 4;
 
         memcpy(&tmp_maybe_address, _buffer, 4);
-        if(tmp_maybe_address == 0){
-            element->ptr->serde_functions.deserialize = NULL;
-            _buffer += 4;
-        } else {
-            memcpy(&element->ptr->serde_functions.deserialize, _buffer, 4);
-            _buffer += 4;
-        }
+        if(tmp_maybe_address != 0) memcpy(&element->ptr->serde_functions.deserialize, _buffer, 4);
+        _buffer += 4;
 
         int arg_len;
         //deserialization of _arg
