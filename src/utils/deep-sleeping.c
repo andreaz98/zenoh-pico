135526d2e97a362b8d49e766f39c86cab1f529a3
andreaz98 <azanni518@gmail.com>
2023-10-16 11:59:52 +0200

    effectively update buffer from the callee to the caller

--- src/utils/deep-sleeping.c
+++ src/utils/deep-sleeping.c
@@ -463,6 +463,7 @@ _z_questionable_sptr_list_t * _deserialize_z_questionable_sptr_list_t(uint8_t * 
 }
 
 int _serialize_z_pending_reply_list_t(_z_pending_reply_list_t * list, uint8_t ** pending_replies){
+    ESP_LOGI("DEBUG", "_serialize_z_pending_reply_list_t\n");
     int ret = _Z_RES_OK;
     _z_pending_reply_t *element;
 
@@ -470,8 +471,8 @@ int _serialize_z_pending_reply_list_t(_z_pending_reply_list_t * list, uint8_t **
     uint8_t *_buffer = *pending_replies;
 
     //Serialization
-    memcpy(_buffer, &no_of_elements, sizeof(size_t));
-    _buffer += sizeof(size_t);
+    memcpy(*pending_replies, &no_of_elements, sizeof(size_t));
+    *pending_replies += sizeof(size_t);
 
     _z_pending_reply_list_t *iterate_list = list;
     while(!_z_pending_reply_list_is_empty(iterate_list)){
@@ -482,27 +483,27 @@ int _serialize_z_pending_reply_list_t(_z_pending_reply_list_t * list, uint8_t **
         //     _z_timestamp_t _tstamp;
         // } _z_pending_reply_t;
         
-        memcpy(_buffer, element->_tstamp.id.id, 16*sizeof(uint8_t));
-        _buffer += 16*sizeof(uint8_t);
+        memcpy(*pending_replies, element->_tstamp.id.id, 16*sizeof(uint8_t));
+        *pending_replies += 16*sizeof(uint8_t);
 
-        memcpy(_buffer, &element->_tstamp.time, sizeof(uint64_t));
-        _buffer += sizeof(uint64_t);
+        memcpy(*pending_replies, &element->_tstamp.time, sizeof(uint64_t));
+        *pending_replies += sizeof(uint64_t);
 
         // typedef struct {
         //     _z_reply_data_t data;
         //     z_reply_tag_t _tag;
         // } _z_reply_t;
 
-        memcpy(_buffer, &element->_reply._tag, sizeof(z_reply_tag_t));
-        _buffer += sizeof(z_reply_tag_t);
+        memcpy(*pending_replies, &element->_reply._tag, sizeof(z_reply_tag_t));
+        *pending_replies += sizeof(z_reply_tag_t);
 
         // typedef struct {
         //     _z_sample_t sample;
         //     _z_id_t replier_id;
         // } _z_reply_data_t;
 
-        memcpy(_buffer, element->_reply.data.replier_id.id, 16*sizeof(uint8_t));
-        _buffer += 16*sizeof(uint8_t);
+        memcpy(*pending_replies, element->_reply.data.replier_id.id, 16*sizeof(uint8_t));
+        *pending_replies += 16*sizeof(uint8_t);
 
         // typedef struct {
         //     _z_keyexpr_t keyexpr;
@@ -512,57 +513,60 @@ int _serialize_z_pending_reply_list_t(_z_pending_reply_list_t * list, uint8_t **
         //     z_sample_kind_t kind;
         // } _z_sample_t;
         _z_sample_t sample = element->_reply.data.sample;
-        memcpy(_buffer, &sample.keyexpr._id, sizeof(uint16_t));
-        _buffer += sizeof(uint16_t);
+        memcpy(*pending_replies, &sample.keyexpr._id, sizeof(uint16_t));
+        *pending_replies += sizeof(uint16_t);
 
-        memcpy(_buffer, &sample.keyexpr._mapping._val, sizeof(uint16_t));
-        _buffer += sizeof(uint16_t);
+        memcpy(*pending_replies, &sample.keyexpr._mapping._val, sizeof(uint16_t));
+        *pending_replies += sizeof(uint16_t);
 
-        memcpy(_buffer, sample.keyexpr._suffix, strlen(sample.keyexpr._suffix) + 1);
-        _buffer += strlen(sample.keyexpr._suffix) + 1;
+        memcpy(*pending_replies, sample.keyexpr._suffix, strlen(sample.keyexpr._suffix) + 1);
+        *pending_replies += strlen(sample.keyexpr._suffix) + 1;
 
-        memcpy(_buffer, &sample.payload._is_alloc, sizeof(_Bool));
-        _buffer += sizeof(_Bool);
+        memcpy(*pending_replies, &sample.payload._is_alloc, sizeof(_Bool));
+        *pending_replies += sizeof(_Bool);
+        ESP_LOGI("DEBUG", "sample.payload._is_alloc %d\n", sample.payload._is_alloc);
 
-        memcpy(_buffer, &sample.payload.len, sizeof(size_t));
-        _buffer += sizeof(size_t);
+        memcpy(*pending_replies, &sample.payload.len, sizeof(size_t));
+        *pending_replies += sizeof(size_t);
+        ESP_LOGI("DEBUG", "sample.payload.len %d\n", sample.payload.len);
 
-        memcpy(_buffer, sample.payload.start, sample.payload.len);
-        _buffer += sample.payload.len;
+        memcpy(*pending_replies, sample.payload.start, sample.payload.len);
+        *pending_replies += sample.payload.len;
 
-        memcpy(_buffer, sample.timestamp.id.id, 16*sizeof(uint8_t));
-        _buffer += 16*sizeof(uint8_t);
+        memcpy(*pending_replies, sample.timestamp.id.id, 16*sizeof(uint8_t));
+        *pending_replies += 16*sizeof(uint8_t);
 
-        memcpy(_buffer, &sample.timestamp.time, sizeof(uint64_t));
-        _buffer += sizeof(uint64_t);
+        memcpy(*pending_replies, &sample.timestamp.time, sizeof(uint64_t));
+        *pending_replies += sizeof(uint64_t);
 
-        memcpy(_buffer, &sample.encoding.suffix._is_alloc, sizeof(_Bool));
-        _buffer += sizeof(_Bool);
+        memcpy(*pending_replies, &sample.encoding.suffix._is_alloc, sizeof(_Bool));
+        *pending_replies += sizeof(_Bool);
 
-        memcpy(_buffer, &sample.encoding.suffix.len, sizeof(size_t));
-        _buffer += sizeof(size_t);
+        memcpy(*pending_replies, &sample.encoding.suffix.len, sizeof(size_t));
+        *pending_replies += sizeof(size_t);
 
-        memcpy(_buffer, sample.encoding.suffix.start, sample.encoding.suffix.len);
-        _buffer += sample.encoding.suffix.len;
+        memcpy(*pending_replies, sample.encoding.suffix.start, sample.encoding.suffix.len);
+        *pending_replies += sample.encoding.suffix.len;
 
-        memcpy(_buffer, &sample.encoding.prefix, sizeof(z_encoding_prefix_t));
-        _buffer += sizeof(z_encoding_prefix_t);
+        memcpy(*pending_replies, &sample.encoding.prefix, sizeof(z_encoding_prefix_t));
+        *pending_replies += sizeof(z_encoding_prefix_t);
 
-        memcpy(_buffer, &sample.kind, sizeof(z_sample_kind_t));
-        _buffer += sizeof(z_sample_kind_t);
+        memcpy(*pending_replies, &sample.kind, sizeof(z_sample_kind_t));
+        *pending_replies += sizeof(z_sample_kind_t);
     }
 
     return ret;
 }
 
 _z_pending_reply_list_t * _deserialize_z_pending_reply_list_t(uint8_t **buffer){
+    ESP_LOGI("DEBUG", "_deserialize_z_pending_reply_list_t\n");
     _z_pending_reply_list_t * list = _z_pending_reply_list_new();
     _z_pending_reply_t *element;
     size_t no_of_elements;
     uint8_t * _buffer = *buffer;
 
     memcpy(&no_of_elements, _buffer, sizeof(size_t));
-    _buffer += sizeof(size_t);
+    *buffer += sizeof(size_t);
 
     for (size_t i = 0; i < no_of_elements; i++){
         element = (_z_pending_reply_t *) malloc(sizeof(_z_pending_reply_t));
@@ -573,27 +577,27 @@ _z_pending_reply_list_t * _deserialize_z_pending_reply_list_t(uint8_t **buffer){
         //     _z_timestamp_t _tstamp;
         // } _z_pending_reply_t;
         
-        memcpy(element->_tstamp.id.id, _buffer, 16*sizeof(uint8_t));
-        _buffer += 16*sizeof(uint8_t);
+        memcpy(element->_tstamp.id.id, *buffer, 16*sizeof(uint8_t));
+        *buffer += 16*sizeof(uint8_t);
 
-        memcpy(&element->_tstamp.time, _buffer, sizeof(uint64_t));
-        _buffer += sizeof(uint64_t);
+        memcpy(&element->_tstamp.time, *buffer, sizeof(uint64_t));
+        *buffer += sizeof(uint64_t);
 
         // typedef struct {
         //     _z_reply_data_t data;
         //     z_reply_tag_t _tag;
         // } _z_reply_t;
 
-        memcpy(&element->_reply._tag, _buffer, sizeof(z_reply_tag_t));
-        _buffer += sizeof(z_reply_tag_t);
+        memcpy(&element->_reply._tag, *buffer, sizeof(z_reply_tag_t));
+        *buffer += sizeof(z_reply_tag_t);
 
         // typedef struct {
         //     _z_sample_t sample;
         //     _z_id_t replier_id;
         // } _z_reply_data_t;
 
-        memcpy(element->_reply.data.replier_id.id, _buffer, 16*sizeof(uint8_t));
-        _buffer += 16*sizeof(uint8_t);
+        memcpy(element->_reply.data.replier_id.id, *buffer, 16*sizeof(uint8_t));
+        *buffer += 16*sizeof(uint8_t);
 
         // typedef struct {
         //     _z_keyexpr_t keyexpr;
@@ -603,49 +607,52 @@ _z_pending_reply_list_t * _deserialize_z_pending_reply_list_t(uint8_t **buffer){
         //     z_sample_kind_t kind;
         // } _z_sample_t;
         _z_sample_t sample = element->_reply.data.sample;
-        memcpy(&sample.keyexpr._id, _buffer, sizeof(uint16_t));
-        _buffer += sizeof(uint16_t);
+        memcpy(&sample.keyexpr._id, *buffer, sizeof(uint16_t));
+        *buffer += sizeof(uint16_t);
 
-        memcpy(&sample.keyexpr._mapping._val, _buffer, sizeof(uint16_t));
-        _buffer += sizeof(uint16_t);
+        memcpy(&sample.keyexpr._mapping._val, *buffer, sizeof(uint16_t));
+        *buffer += sizeof(uint16_t);
 
-        sample.keyexpr._suffix = (char *)malloc(strlen((char *)_buffer) + 1);
-        memcpy(sample.keyexpr._suffix, _buffer, strlen((char *)_buffer) + 1);
-        _buffer += strlen((char *)_buffer) + 1;
+        sample.keyexpr._suffix = (char *)malloc(strlen((char *)*buffer) + 1);
+        memcpy(sample.keyexpr._suffix, *buffer, strlen((char *)*buffer) + 1);
+        *buffer += strlen((char *)*buffer) + 1;
 
-        memcpy(&sample.payload._is_alloc, _buffer,  sizeof(_Bool));
-        _buffer += sizeof(_Bool);
+        memcpy(&sample.payload._is_alloc, *buffer,  sizeof(_Bool));
+        *buffer += sizeof(_Bool);
+        ESP_LOGI("DEBUG", "sample.payload._is_alloc %d\n", sample.payload._is_alloc);
 
-        memcpy(&sample.payload.len, _buffer, sizeof(size_t));
-        _buffer += sizeof(size_t);
+        memcpy(&sample.payload.len, *buffer, sizeof(size_t));
+        *buffer += sizeof(size_t);
 
         //raises warning: expected 'void * restrict' but argument is of type 'const uint8_t *'
+        ESP_LOGI("DEBUG", "sample.payload.len %d\n", sample.payload.len);
         sample.payload.start = (uint8_t *)malloc(sample.payload.len);
-        memcpy(sample.payload.start, _buffer, sample.payload.len);
-        _buffer += sample.payload.len;
+        ESP_LOGI("DEBUG", "sample.payload.start %p\n", sample.payload.start);
+        memcpy(sample.payload.start, *buffer, sample.payload.len);
+        *buffer += sample.payload.len;
 
-        memcpy(sample.timestamp.id.id, _buffer, 16*sizeof(uint8_t));
-        _buffer += 16*sizeof(uint8_t);
+        memcpy(sample.timestamp.id.id, *buffer, 16*sizeof(uint8_t));
+        *buffer += 16*sizeof(uint8_t);
 
-        memcpy(&sample.timestamp.time, _buffer, sizeof(uint64_t));
-        _buffer += sizeof(uint64_t);
+        memcpy(&sample.timestamp.time, *buffer, sizeof(uint64_t));
+        *buffer += sizeof(uint64_t);
 
-        memcpy(&sample.encoding.suffix._is_alloc, _buffer, sizeof(_Bool));
-        _buffer += sizeof(_Bool);
+        memcpy(&sample.encoding.suffix._is_alloc, *buffer, sizeof(_Bool));
+        *buffer += sizeof(_Bool);
 
-        memcpy(&sample.encoding.suffix.len, _buffer, sizeof(size_t));
-        _buffer += sizeof(size_t);
+        memcpy(&sample.encoding.suffix.len, *buffer, sizeof(size_t));
+        *buffer += sizeof(size_t);
 
         //raises warning: expected 'void * restrict' but argument is of type 'const uint8_t *'
         sample.encoding.suffix.start = (uint8_t *)malloc(sample.encoding.suffix.len);
-        memcpy(sample.encoding.suffix.start, _buffer, sample.encoding.suffix.len);
-        _buffer += sample.encoding.suffix.len;
+        memcpy(sample.encoding.suffix.start, *buffer, sample.encoding.suffix.len);
+        *buffer += sample.encoding.suffix.len;
 
-        memcpy(&sample.encoding.prefix, _buffer, sizeof(z_encoding_prefix_t));
-        _buffer += sizeof(z_encoding_prefix_t);
+        memcpy(&sample.encoding.prefix, *buffer, sizeof(z_encoding_prefix_t));
+        *buffer += sizeof(z_encoding_prefix_t);
 
-        memcpy(&sample.kind, _buffer, sizeof(z_sample_kind_t));
-        _buffer += sizeof(z_sample_kind_t);
+        memcpy(&sample.kind, *buffer, sizeof(z_sample_kind_t));
+        *buffer += sizeof(z_sample_kind_t);
 
         _z_pending_reply_list_push(list, element);
     }
@@ -654,6 +661,7 @@ _z_pending_reply_list_t * _deserialize_z_pending_reply_list_t(uint8_t **buffer){
 }
 
 int _serialize_z_pending_query_list_t(_z_pending_query_list_t *list, int8_t (*write_call_arg)(void *writer, const char *serialized, int serialized_len), int8_t (*write_drop_arg)(void *writer, const char *serialized, int serialized_len), uint8_t *pending_queries){
+    ESP_LOGI("DEBUG", "_serialize_z_pending_query_list_t\n");
     int ret = _Z_RES_OK;
     _z_pending_query_t *element;
 
@@ -695,28 +703,31 @@ int _serialize_z_pending_query_list_t(_z_pending_query_list_t *list, int8_t (*wr
         memcpy(_buffer, &element->_anykey, sizeof(_Bool));
         _buffer += sizeof(_Bool);
 
+        ESP_LOGI("DEBUG", "pre &_buffer: %p", _buffer);
         _serialize_z_pending_reply_list_t(element->_pending_replies, &_buffer);// &_buffer to update the buffer pointer from the callee to the caller (I.E. this function)
+        ESP_LOGI("DEBUG", "post &_buffer: %p", _buffer);
+        // memcpy(_buffer, &element->_callback, 4); //cannot be NULL
+        // _buffer += 4;
 
-        memcpy(_buffer, &element->_callback, 4); //cannot be NULL
-        _buffer += 4;
+        // // troubleshooting IllegalInstruction
+        // if(element->_dropper != NULL) memcpy(_buffer, &element->_dropper, 4);
+        // _buffer += 4;
 
-        if(element->_dropper != NULL) memcpy(_buffer, &element->_dropper, 4);
-        _buffer += 4;
+        // if(element->serde_functions.serialize != NULL) memcpy(_buffer, &element->serde_functions.serialize, 4);
+        // _buffer += 4;
 
-        if(element->serde_functions.serialize != NULL) memcpy(_buffer, &element->serde_functions.serialize, 4);
-        _buffer += 4;
+        // if(element->serde_functions.deserialize != NULL) memcpy(_buffer, &element->serde_functions.deserialize, 4);
+        // _buffer += 4;
 
-        if(element->serde_functions.deserialize != NULL) memcpy(_buffer, &element->serde_functions.deserialize, 4);
-        _buffer += 4;
+        // // element->serde_functions.serialize != NULL solves InstrFetchProhibited when element->_call_arg != NULL but no serialize function has been provided by the user.
+        // if(element->_call_arg != NULL && element->serde_functions.serialize != NULL) element->serde_functions.serialize(write_call_arg, &_buffer, element->_call_arg);
 
-        // element->serde_functions.serialize != NULL solves InstrFetchProhibited when element->_call_arg != NULL but no serialize function has been provided by the user.
-        if(element->_call_arg != NULL && element->serde_functions.serialize != NULL) element->serde_functions.serialize(write_call_arg, &_buffer, element->_call_arg);
-
-        if(element->_drop_arg != NULL && element->serde_functions.serialize != NULL) element->serde_functions.serialize(write_drop_arg, &_buffer, element->_drop_arg);
+        // if(element->_drop_arg != NULL && element->serde_functions.serialize != NULL) element->serde_functions.serialize(write_drop_arg, &_buffer, element->_drop_arg);
         
         iterate_list = _z_pending_query_list_tail(iterate_list);
     }
 
+    ESP_LOGI("DEBUG", "right before ret\n");
     return ret;
 }
 
@@ -753,6 +764,7 @@ _z_pending_query_list_t * _deserialize_z_pending_query_list_t(uint8_t *buffer){
         memcpy(element->_parameters, _buffer, strlen((char *)_buffer) + 1);
         _buffer += strlen((char *)_buffer) + 1;
 
+
         memcpy(&element->_target, _buffer, sizeof(z_query_target_t));
         _buffer += sizeof(z_query_target_t);
 
@@ -762,38 +774,40 @@ _z_pending_query_list_t * _deserialize_z_pending_query_list_t(uint8_t *buffer){
         memcpy(&element->_anykey, _buffer, sizeof(_Bool));
         _buffer += sizeof(_Bool);
 
+        ESP_LOGI("DEBUG", "pre &_buffer: %p", _buffer);
         element->_pending_replies = _deserialize_z_pending_reply_list_t(&_buffer);
+        ESP_LOGI("DEBUG", "post &_buffer: %p", _buffer);
 
-        memcpy(&element->_callback, _buffer, 4); //cannot be NULL
-        _buffer += 4;
+        // memcpy(&element->_callback, _buffer, 4); //cannot be NULL
+        // _buffer += 4;
 
-        size_t tmp_maybe_address;
-        memcpy(&tmp_maybe_address, _buffer, 4);
-        if(tmp_maybe_address != 0) memcpy(&element->_dropper, _buffer, 4);
-        _buffer += 4;
+        // size_t tmp_maybe_address;
+        // memcpy(&tmp_maybe_address, _buffer, 4);
+        // if(tmp_maybe_address != 0) memcpy(&element->_dropper, _buffer, 4);
+        // _buffer += 4;
 
-        memcpy(&tmp_maybe_address, _buffer, 4);
-        if(tmp_maybe_address != 0) memcpy(&element->serde_functions.serialize, _buffer, 4);
-        _buffer += 4;
+        // memcpy(&tmp_maybe_address, _buffer, 4);
+        // if(tmp_maybe_address != 0) memcpy(&element->serde_functions.serialize, _buffer, 4);
+        // _buffer += 4;
 
-        memcpy(&tmp_maybe_address, _buffer, 4);
-        if(tmp_maybe_address != 0) memcpy(&element->serde_functions.deserialize, _buffer, 4);
-        _buffer += 4;
+        // memcpy(&tmp_maybe_address, _buffer, 4);
+        // if(tmp_maybe_address != 0) memcpy(&element->serde_functions.deserialize, _buffer, 4);
+        // _buffer += 4;
 
-        int _call_arg_len;
-        int _drop_arg_len;
-        //deserialization of _call_arg & _drop_arg
-        if(element->serde_functions.deserialize != NULL){
-            memcpy(&_call_arg_len, _buffer, sizeof(int));
-            _buffer += sizeof(int);
-            element->serde_functions.deserialize((char *)_buffer, _call_arg_len, (void **)&"call_arg");
-            _buffer += _call_arg_len;
+        // int _call_arg_len;
+        // int _drop_arg_len;
+        // //deserialization of _call_arg & _drop_arg
+        // if(element->serde_functions.deserialize != NULL){
+        //     memcpy(&_call_arg_len, _buffer, sizeof(int));
+        //     _buffer += sizeof(int);
+        //     element->serde_functions.deserialize((char *)_buffer, _call_arg_len, (void **)&"call_arg");
+        //     _buffer += _call_arg_len;
 
-            memcpy(&_drop_arg_len, _buffer, sizeof(int));
-            _buffer += sizeof(int);
-            element->serde_functions.deserialize((char *)_buffer, _drop_arg_len, (void **)&"drop_arg");
-            _buffer += _drop_arg_len;
-        }
+        //     memcpy(&_drop_arg_len, _buffer, sizeof(int));
+        //     _buffer += sizeof(int);
+        //     element->serde_functions.deserialize((char *)_buffer, _drop_arg_len, (void **)&"drop_arg");
+        //     _buffer += _drop_arg_len;
+        // }
 
         _z_pending_query_list_push(list, element);
     }
